a8b9adeddb7f961a956585b9024049dceb29920a
André Pereira <andrepereira97@yahoo.com>
2024-01-27 09:43:13 -0300

    :tada: init project

---
+++ src/main/java/org/example/rhytspringboot/RhYtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.rhytspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhYtSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RhYtSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/rhytspringboot/RhYtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rhytspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhYtSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
