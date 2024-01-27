370bbbb3075b38c4f82c24edfa036badb1e13ae1
André Pereira <andrepereira97@yahoo.com>
2024-01-27 09:52:57 -0300

    :sparkles: add hello world route

---
+++ src/main/java/org/example/rhytspringboot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.example.rhytspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+  @GetMapping
+  public Map<String, ?> home() {
+    return Map.of("message", "Hello World!");
+  }
+}
